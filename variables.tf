77e255f546e091c45401f7348d2ab3ab9d5362af
Jason Smith <jason@innovationinsoftware.com>
2025-07-29 18:24:43 -0700

    HCP test

--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
 # Variable declarations
+variable "instance_type" {
+  description = "EC2 instance type AWS"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances to create"
+  type        = number
+}
